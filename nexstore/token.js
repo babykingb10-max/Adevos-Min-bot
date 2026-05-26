10f00a92f3887da93f7ade6c8cca1af0e095a526
𝐀𝐃𝐄𝐕𝐎𝐒-𝐗⁩ <babykingb10@gmail.com>
2026-05-26 14:45:56 +0300

    Update token.js

--- nexstore/token.js
+++ nexstore/token.js
@@ -1,9 +1,5 @@
 
 module.exports = {
-  BOT_TOKEN: //'8713612176:AAFPT4X9HrQPtdVJC1G3jWm8olstwPulXpE',  //server two
-  //'8588113669:AAGI3ZD8PY1dyRRmHCexgbpbCyN3k37Yio8', //server one
-  //'8603443296:AAETAdzORw-Qk6E1iyRo3gk80nZ-vFkXZPk', //server three
-  //'8761279582:AAG_yFnSp2V7jb0kr0MNi4APROryalr3wRo', //server four
-  //'8764310616:AAFyXZwWUzB_pHtwzKrGnJrZRYIHinM997I', //server five
+  BOT_TOKEN: '8554252676:AAE52VhmLq_QfRnEzDHYLf0mhxUaTjyg7qg',  //Upgrade tes
   startupPassword: 'nexus'
 };
