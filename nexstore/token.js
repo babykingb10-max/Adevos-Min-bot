61d557a9fc6b4698e73bd86f753512726fb174ef
𝐀𝐃𝐄𝐕𝐎𝐒-𝐗⁩ <babykingb10@gmail.com>
2026-05-26 12:28:23 +0300

    Update token.js with BOT_TOKEN comments
    Commented out multiple BOT_TOKEN values and added a startupPassword.

--- nexstore/token.js
+++ nexstore/token.js
@@ -1,1 +1,9 @@
 
+module.exports = {
+  BOT_TOKEN: //'8713612176:AAFPT4X9HrQPtdVJC1G3jWm8olstwPulXpE',  //server two
+  //'8588113669:AAGI3ZD8PY1dyRRmHCexgbpbCyN3k37Yio8', //server one
+  //'8603443296:AAETAdzORw-Qk6E1iyRo3gk80nZ-vFkXZPk', //server three
+  //'8761279582:AAG_yFnSp2V7jb0kr0MNi4APROryalr3wRo', //server four
+  //'8764310616:AAFyXZwWUzB_pHtwzKrGnJrZRYIHinM997I', //server five
+  startupPassword: 'nexus'
+};
